e3de3420ab2267b9c63faa567cbf55cd69703fc2
limaluan <limaluan.dev@gmail.com>
2022-03-06 14:44:15 -0300

    adds: client already exists verificaction

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
 const app = express();
-
-var customers = [];
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.json());
 
+const customers = [];
+
 app.post("/account", (req, res) => {
     const { cpf, name } = req.body;
-    const id = uuidv4();
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer already exists." })
+    }
+
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
-
     console.log(customers);
+
     return res.status(201).send();
-});
+})
 
 app.listen(3000, (err) => {
     if (!err) {
