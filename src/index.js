1e4f74f8324534e824baec088e238e747b041a97
limaluan <limaluan.dev@gmail.com>
2022-03-06 20:07:39 -0300

    adds: creates route to make deposits

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ app.use(express.json());
 
 const customers = [];
 
+// Middleware
+function verifyIfAccountExists(req, res, next) {
+    const { cpf } = req.headers;
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(401).json({ error: "Customer not found" });
+    }
+
+    req.customer = customer; // Declarando que o customer para requisição é o customer
+
+    return next();
+}
+
 app.post("/account", (req, res) => {
-    const { cpf, name } = req.body;
+    const { cpf, name } = req.headers;
 
     const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
 
@@ -26,11 +40,23 @@ app.post("/account", (req, res) => {
     return res.status(201).send();
 })
 
-app.get("/statement/:cpf", (req, res) => {
-    const { cpf } = req.params;
-    const customer = customers.find((customer) => customer.cpf === cpf);
+app.get("/statement", verifyIfAccountExists, (req, res) => {
+    const { customer } = req;
 
     return res.json(customer.statement);
+})
+
+app.post("/deposit", verifyIfAccountExists, (req, res) => {
+    const { description, ammount } = req.body;
+    const { customer } = req;
+
+    customer.statement.push({
+        description,
+        ammount,
+        date: new Date(),
+        type: "credit"
+    });
+    return res.status(201).send();
 })
 
 app.listen(3000, (err) => {
